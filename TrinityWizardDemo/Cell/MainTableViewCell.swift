d066c45c547563392fd31e3758ce565232fdd305
Umi Amira <umiamiraazhar@gmail.com>
2024-03-22 21:18:02 +0800

    Initial commit

---
+++ TrinityWizardDemo/Cell/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  TrinityWizardDemo
+//
+//  Created by Umi Amira on 22/03/2024.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
